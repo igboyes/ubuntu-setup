c7d5ce8fa906c1cf2af2dc5f3772637dd856dcd5
Ian Boyes <igboyes@gmail.com>
2022-01-06 13:47:31 -0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,74 @@
+# APT
+sudo apt update
+sudo apt remove libreoffice* rhythmbox thunderbird
+sudo apt autoremove
+sudo apt uprade
+sudo apt install apt-transport-https ca-certificates curl git gnupg iotop lsb-release neovim openssh-server python-is-python3 python3-venv python3-dev software-properties-common tree wget
+
+# Docker
+sudo apt update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+
+# Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update
+sudo apt install terraform
+terraform -install-autocomplete
+
+# Azure CLI
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt update
+sudo apt install azure-cli
+
+# GitHub CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
+# Poetry
+curl -sSL https://install.python-poetry.org | python3 -
+echo "PATH=${PATH}:${HOME}/.local/bin" >> ~/.bashrc
+poetry completions bash > /etc/bash_completion.d/poetry
+
+# Clone Virtool
+mkdir $HOME/Projects
+(
+    cd $HOME/Projects
+    git clone https://github.com/virtool/virtool.git
+    git clone https://github.com/virtool/virtool-ui.git
+    git clone https://github.com/virtool/virtool-workflow.git
+)
+
+# Chrome
+(
+    cd $HOME/Downloads
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install ./google-chrome-stable_current_amd64.deb
+)
+
+# VSCode
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
+
+# Snap
+sudo snap install slack --classic
+sudo snap install pycharm-professional --classic
